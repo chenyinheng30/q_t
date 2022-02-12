2e89acb4e81ed5627309ad8633ddc026f710aaad
chenyinheng30 <chenyinheng30@outlook.com>
2022-02-12 14:58:58 +0800

    完成running_by_cycle主要功能的设计，准备为running_by_cycle的线程设置返回值

--- q_t.py
+++ q_t.py
@@ -1,24 +1,21 @@
 '''一个计时器'''
-
-from concurrent.futures import ThreadPoolExecutor
-from itertools import cycle
+from queue import Queue
 from threading import Thread
 import time
 def fun_time_butler(start_time,accuracy, func,args=None):
-    #print('fun_time_butler::'+start_time)
     delta_time=start_time-time.time()
-    if delta_time>0:
+    if delta_time>3*accuracy:
         sleep_time=delta_time-3*accuracy
         time.sleep(sleep_time)
-        while(time.time()<start_time):
-            pass
-        now_time=time.time()
-        if abs(now_time-start_time)>accuracy:
-            raise RuntimeError('time_butler: outtime error:'+str(abs(now_time-start_time)))
-            return
     elif delta_time<-accuracy:
         raise RuntimeError('time_butler: outtime error:'+str(abs(delta_time)))
         return
+    while(time.time()<start_time):
+        pass
+    # now_time=time.time()
+    # if abs(now_time-start_time)>accuracy:
+    #     raise RuntimeError('time_butler: outtime error:'+str(abs(now_time-start_time)))
+    #     return
     func(*args)
 class time_butler(Thread):
     def __init__(self,start_time,accuracy, func,args=None):
@@ -30,10 +27,20 @@ class time_butler(Thread):
     def run(self):
         fun_time_butler(self.__start_time,self.__accuracy,self.__func,self.__args)
 class running_by_cycle(Thread):
-    def __init__(self,start_time,cycle,running_time,accuracy,func,args=None) -> None:
+    def __init__(self,start_time,cycle,running_time,accuracy,func,args):
+        Thread.__init__(self)
+        self.__tasks_queue=Queue(maxsize=running_time/cycle)
+        self.__result=None
         self.__start_time=start_time
-        self.__cycle=cycle
-        self.__running_time=running_time
-        self.__accuracy=accuracy
-        self.__func=func
-        self.__args=args
+        for t in range(0,running_time,cycle):
+            task=time_butler(start_time+t,accuracy,func,tuple([self.__result]+list(args)))
+            self.__tasks_queue.put(task)
+    def run(self):
+        len=self.__tasks_queue.qsize()
+        self.__result=Queue(len)
+        for i in range(len):
+            task=self.__tasks_queue.get()
+            task.start()
+            task.join()
+    def get_result(self):
+        return self.__result
